033d01d97d6eededab6c13988ff3df9821987df3
Harshita <hgupta5_be18@thapar.edu>
2023-11-05 14:31:01 +0530

    form validation added

--- logic.js
+++ logic.js
@@ -1,46 +1,101 @@
-let totalBill = 0;
-let selectedTip = 0;
-let totalPeople = 0;
-
-document.getElementById("bill").addEventListener("input",(e)=>{
-    totalBill = parseInt(e.target.value);
-    console.log(totalBill);
-});
-
-document.querySelector(".tip-buttons #btn1").addEventListener("click",(e)=>{
-    selectedTip = parseInt(e.target.innerText);
-    console.log(typeof(selectedTip));
-});
-
-document.querySelector("#number-of-people").addEventListener("input",(e)=>{
-    totalPeople = parseInt(e.target.value);
-    console.log(totalPeople);
-});
-
-document.querySelector("#calculate-tip").addEventListener("click",(e)=>{
-    let tipPerPerson = 0;
-    let totalTip = (selectedTip/100)*totalBill;
-    tipPerPerson = totalTip/totalPeople;
-
-    let totalPerPerson = 0;
-    totalPerPerson = totalBill/totalPeople;
-
-    document.querySelector(".tip-info1-value").innerHTML = tipPerPerson;
-    document.querySelector(".total-info2-value").innerHTML = totalPerPerson;
-});
 
 
-document.querySelector("#reset-btn").addEventListener("click",(e)=>{
-    tipPerPerson = 0;
-    totalPerPerson = 0;
-    totalBill = 0;
+
+
+// document.querySelector(".tip-buttons #btn1").addEventListener("click", (e) => {
+//   selectedTip = parseInt(e.target.innerText);
+//   console.log(selectedTip);
+// });
+const btns = document.querySelector(".tip-buttons");
+    let selectedTip = 0;
+    btns.addEventListener("click", (e) => {
+        console.log(e.target.id);
+        let selectedBtnId = e.target.id;
+        selectedTip = parseInt(e.target.innerText);
+        document.getElementById(selectedBtnId).classList.add("selectedBtn");
+        //add the selectedBtn class to the current element being selected and remove from other elements if present
+        console.log(selectedTip);
+        for(let item of btns.children){
+            console.log("id", item.id);
+            if(item.id != selectedBtnId && item.classList.contains('selectedBtn')){
+                item.classList.remove('selectedBtn');
+            }
+        }
+    });
+
+
+function resetForm(){
+    document.querySelector("#tip-calculator-form").reset();
+    clearErrors();
+    for(let item of btns.children){
+        item.classList.remove('selectedBtn');
+        item.value = 0;  
+    }
     selectedTip = 0;
-    totalPeople = 0;
-    document.querySelector("#number-of-people").value = totalPeople;
-    document.getElementById("bill").value = totalBill;
+    document.querySelector(".tip-info1-value").innerHTML = 0;
+    document.querySelector(".total-info2-value").innerHTML = 0;
+}
+
+function calculateTotalTip(isFormValidated,ftotalBill,ftotalPeople) {
+  if (isFormValidated) {
+    //calcuate the totals else let it remain to 0
+    let tipPerPerson = 0;
+    let totalPerPerson = 0;
+    let totalTip = (selectedTip / 100) * ftotalBill;
+    tipPerPerson = totalTip / ftotalPeople;
+    totalPerPerson = ftotalBill / ftotalPeople;
+
     document.querySelector(".tip-info1-value").innerHTML = tipPerPerson;
     document.querySelector(".total-info2-value").innerHTML = totalPerPerson;
-});
+  } else {
+    document.querySelector(".tip-info1-value").innerHTML = 0;
+    document.querySelector(".total-info2-value").innerHTML = 0;
+  }
+}
+
+function clearErrors() {
+  let errors = document.getElementsByClassName("form-error");
+  console.log(errors);
+  for (let item of errors) {
+    item.innerText = "";
+  }
+}
+
+function setErrors(id, error) {
+  let element = document.getElementById(id);
+  console.log("element");
+  element.getElementsByClassName("form-error")[0].innerText = error;
+}
+
+function validateForm(e) {
+  e.preventDefault();
+  let returnValue = true;
+  clearErrors();
+
+  let form = document.querySelector("#tip-calculator-form");
+  let inputBill = form["bill"];
+  let totalPeopleInput = form["number-of-people"]
 
 
+  if (inputBill.value <= 0) {
+    setErrors("fbill", "Please enter amount greater than 0.");
+    returnValue = false;
+  }
+  if (inputBill.value > 10000) {
+    setErrors(
+      "fbill",
+      "Accepting amount till 10K only.Please enter valid amount."
+    );
+    returnValue = false;
+  }
+  if(selectedTip == 0){
+    setErrors("tip-buttons-form","Please select a tip value.")
+  }
+  if (totalPeopleInput.value <= 0) {
+    setErrors("ftotal-people", "Number of people cannot be 0 or negative.");
+    returnValue = false;
+  }
 
+  calculateTotalTip(returnValue,inputBill.value,totalPeopleInput.value);
+  return returnValue;
+}
