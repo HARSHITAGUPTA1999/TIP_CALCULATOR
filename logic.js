8e51c445fb7bcd0543484abfdbceab634385fc51
Harshita <hgupta5_be18@thapar.edu>
2023-11-11 18:00:53 +0530

    features section added

--- logic.js
+++ logic.js
@@ -1,4 +1,5 @@
 const btns = document.querySelector(".tip-buttons");
+const featureList = document.querySelector("#list");
 let selectedTip = 0;
 
 //creating the tip selectors btns
@@ -9,6 +10,37 @@ function createTipSelectors() {
     btn.setAttribute("id", `btn${index + 1}`);
     btn.classList.add("btn","btn-primary","tip-selector-btn");
     btns.appendChild(btn);
+  }
+}
+
+//creating the features section
+function createFeaturesSection() {
+  for (let [index, value] of features.entries()) {
+    let li = document.createElement('li');
+    let div = document.createElement('div');
+    let span = document.createElement('span');
+    let heading = document.createElement('h5');
+    let description = document.createElement('p');
+    let listSeparator = document.createElement('li');
+
+    div.append(heading);
+    div.append(description);
+   
+    li.append(span);
+    li.append(div);
+   
+    li.classList.add("feature");
+    span.classList.add("icon");
+    description.classList.add("description");
+    listSeparator.classList.add("separator");
+    
+    heading.innerText = value.heading;
+    description.innerText = value.desc;
+    span.innerHTML = '&#10003';
+
+    featureList.appendChild(li);
+    featureList.appendChild(listSeparator);
+
   }
 }
 
@@ -37,8 +69,8 @@ function resetForm() {
     item.value = 0;
   }
   selectedTip = 0;
-  document.querySelector(".tip-info1-value").innerHTML = 0;
-  document.querySelector(".total-info2-value").innerHTML = 0;
+  document.querySelector(".tip-info1-value").innerHTML = '$ '+ 0;
+  document.querySelector(".total-info2-value").innerHTML = '$ ' + 0;
 }
 
 //calculate totals and show to user on screen
@@ -48,11 +80,11 @@ function calculateTotalTip(isFormValidated, ftotalBill, ftotalPeople) {
     let tipPerPerson = 0;
     let totalPerPerson = 0;
     let totalTip = (selectedTip / 100) * ftotalBill;
-    tipPerPerson = totalTip / ftotalPeople;
-    totalPerPerson = ftotalBill / ftotalPeople;
+    tipPerPerson = parseFloat(totalTip / ftotalPeople).toFixed(2);
+    totalPerPerson = parseFloat(ftotalBill / ftotalPeople).toFixed(2);
 
-    document.querySelector(".tip-info1-value").innerHTML = tipPerPerson;
-    document.querySelector(".total-info2-value").innerHTML = totalPerPerson;
+    document.querySelector(".tip-info1-value").innerHTML ='$ '+ tipPerPerson;
+    document.querySelector(".total-info2-value").innerHTML = '$ ' + totalPerPerson;
   } else {
     document.querySelector(".tip-info1-value").innerHTML = 0;
     document.querySelector(".total-info2-value").innerHTML = 0;
@@ -108,7 +140,54 @@ function validateForm(e) {
   return returnValue;
 }
 
+ // JavaScript to toggle the display of the features section
+function toggleFeaturesSection() {
+  let tipCalculator = document.getElementsByClassName("app-container");
+  // console.log()
+  let featuresSection = document.getElementById('features-section');
+  let app = document.getElementsByClassName('app');
+
+  tipCalculator[0].classList.toggle('hide-calculator');
+  featuresSection.classList.toggle('show-features'); 
+  featuresSection.classList.toggle('hide-feature-section');
+  app[0].classList.toggle('features-body');
+  document.getElementById('menu-icon').classList.toggle('active'); 
+}
+
+
+document.getElementById('menu-icon').addEventListener('click', toggleFeaturesSection);
+//by default feature section will be hidden only
+document.getElementById('features-section').classList.add('hide-feature-section');
+
 const tipPercentages = ["5%", "10%", "15%", "25%", "50%"];
+const features = [
+  {
+    heading: 'Responsive design',
+    desc: 'Responsive web design using HTML and CSS.'
+  },
+  {
+    heading: 'Form creation',
+    desc: 'Implementation of a form in javascript.'
+  },
+  {
+    heading: 'Error handling',
+    desc: 'Showing error states and messages when user enters incomplete or invalid info.'
+  },
+  {
+    heading: 'Tip Selectors functionality',
+    desc: 'Giving the user different tip options. HIghlighting the current tip selected by user.'
+  },
+  {
+    heading: 'Calcuating totals',
+    desc: 'Calculating and displaying totals on successfull form submission.The totals are displaced upto 2 decimal places only.'
+  },
+  {
+    heading: 'Form reset',
+    desc: 'Reseting the form to initial state on clicking of reset button.'
+  },
+
+]
 
 createTipSelectors();
+createFeaturesSection();
 getSelectedTip();
